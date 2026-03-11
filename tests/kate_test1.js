e2a476ffa1067f38799951ccf61b13a2a2518770
Katiuk, Kateryna <kkatiuk@deloitte.com>
2026-03-11 23:07:05 +0100

    add test

--- tests/kate_test1.js
+++ tests/kate_test1.js
@@ -1,1 +1,25 @@
 //test1
+const newArray = [11,2,34];
+let result = 0;
+for (let i=0; i < newArray.length; i++){
+    result= result + newArray[i];
+}
+
+    console.log(result);
+
+//test 2
+let newArray1 = [];
+for (element of newArray){
+    newArray1.push(element + 1);
+}
+
+console.log(newArray1);
+
+//test3
+let newArray2 = newArray1.filter(function(element){
+    return element > 10
+})
+console.log(newArray2);
+
+let newArray3 = newArray1.filter((element) => element > 10);
+console.log(newArray3);
